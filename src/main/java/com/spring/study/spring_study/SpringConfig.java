885552c67f1e0cbaa4ca2421cfacd7a2f78cc863
강대원 <kangdaewon@gangdaewon-ui-MacBookAir.local>
2022-06-27 17:56:41 +0900

    remind jdbcTemplate

--- src/main/java/com/spring/study/spring_study/SpringConfig.java
+++ src/main/java/com/spring/study/spring_study/SpringConfig.java
@@ -6,20 +6,20 @@ import com.spring.study.spring_study.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//
-////java 코드로 bean 등록하는 법
-//@Configuration
-//public class SpringConfig {
-//
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    }
+
+//java 코드로 bean 등록하는 법
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 // db를 바꿀경우 여기만 손대면 컨트롤러, 서비스단은 건들이지 않아도 되는 장점이있다
-//}
+}
--- src/main/java/com/spring/study/spring_study/service/MemberService.java
+++ src/main/java/com/spring/study/spring_study/service/MemberService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 @RequiredArgsConstructor
 @Slf4j
 public class MemberService {
